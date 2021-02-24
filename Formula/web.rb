f1d77eab168bb836c212759295d992eefe8973fd
Lucas Garron <code@garron.net>
2021-02-23 19:43:23 -0800

    Add `web` scripts.

---
+++ Formula/web.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class Web < Formula
+  desc '🌐 Web scripts'
+  homepage 'https://github.com/lgarron/scripts'
+  head 'https://github.com/lgarron/scripts.git'
+
+  def install
+    bin.install 'web/chrometab'
+    bin.install 'web/weblocify'
+  end
+
+  test do
+    system 'false'
+  end
+end
