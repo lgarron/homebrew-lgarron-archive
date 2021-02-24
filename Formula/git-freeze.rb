1817c4913719033367c0c14386572a319b88d184
Lucas Garron <code@garron.net>
2021-02-23 19:21:38 -0800

    Fix `git-freeze` formula.

--- Formula/git-freeze.rb
+++ Formula/git-freeze.rb
@@ -3,11 +3,7 @@
 class GitFreeze < Formula
   desc '❄️ Freeze and 🔥 thaw.'
   homepage 'https://github.com/lgarron/git-freeze'
-  head 'https://github.com/lgarron/git-freeze/archive/main.zip'
-
-  # TODO: Either add a semantic Java dependency (turn this into a cask?) or
-  # print a notification about it.
-  # depends_on cask: "java"
+  head 'https://github.com/lgarron/git-freeze.git'
 
   def install
     bin.install 'bin/git-freeze'
