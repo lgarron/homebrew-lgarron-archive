b2ee607a08b885698ab3f3eb88898e6c253edd47
Lucas Garron <code@garron.net>
2021-03-18 22:48:27 -0700

    Add `system`.

---
+++ Formula/system.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class System < Formula
+  desc '💻 System/shell tools'
+  homepage 'https://github.com/lgarron/scripts'
+  head 'https://github.com/lgarron/scripts.git', :branch => 'main'
+
+  def install
+    bin.install 'system/map'
+    bin.install 'system/unixtime'
+  end
+
+  test do
+    system 'false'
+  end
+end
