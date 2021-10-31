55f0b6566552c0cfc35f6ceb6b67cb943757fefe
Lucas Garron <code@garron.net>
2021-10-31 02:36:24 -0700

    [sd-card-backup] Add `sd-card-backup` (built using Go!)

---
+++ Formula/sd-card-backup.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class SdCardBackup < Formula
+  desc '💻 A simple tool to backup up SD cards.'
+  homepage 'https://github.com/lgarron/sd-card-backup'
+  head 'https://github.com/lgarron/sd-card-backup.git', :branch => 'main'
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args, "./cmd/sd-card-backup"
+  end
+
+  test do
+    system 'false'
+  end
+end
