9620b6e0ad2f9e748febf6f47f61209a600a2fd8
Lucas Garron <code@garron.net>
2022-01-10 16:49:11 -0800

    [ts] Add `ts`.

---
+++ Formula/ts.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class Ts < Formula
+  desc '📜 Run a TypeScript entry file directly, using `esbuild`'
+  homepage 'https://github.com/lgarron/scripts'
+  head 'https://github.com/lgarron/scripts.git', :branch => 'main'
+
+  # TODO: declare a dep on `node` and `fish`?
+
+  def install
+    bin.install 'web/ts'
+  end
+end
