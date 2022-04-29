7d1c65e9912871a4fed039eade9395b854b1bf7b
Lucas Garron <code@garron.net>
2022-04-29 02:28:23 -0700

    Add `maestral-dbx`.

---
+++ Formula/maestral-dbx.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class MaestralDbx < Formula
+  desc '🏋️ dbx-link (get link to file) and dbx-web (visit file on web)'
+  homepage 'https://github.com/lgarron/scripts'
+  head 'https://github.com/lgarron/scripts.git', :branch => 'main'
+
+  def install
+    bin.install 'maestral/dbx-link'
+    bin.install 'maestral/dbx-web'
+  end
+
+  test do
+    system 'false'
+  end
+end
