d3d28c1293dfc424141c7dff885d4b28e685905f
Lucas Garron <code@garron.net>
2023-09-06 15:09:06 -0700

    Change `add-rome` to `add-biome`.

--- Formula/add-biome.rb
+++ Formula/add-biome.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
-class AddRome < Formula
-  desc '🛠️ Add rome to a JS project.'
+class AddBiome < Formula
+  desc '🛠️ Add @biomejs/biome to a JS project.'
   homepage 'https://github.com/lgarron/scripts'
   head 'https://github.com/lgarron/scripts.git', :branch => 'main'
 
   def install
-    bin.install 'web/add-rome'
+    bin.install 'web/add-biome'
   end
 
   test do
