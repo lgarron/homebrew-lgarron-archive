df48fc873bfc2b4e509ba571ae6e4dba6ed76ec7
Lucas Garron <code@garron.net>
2024-10-10 18:42:19 -0700

    [openscad-auto] Add `terminal-notifier` as a dep.

--- Formula/openscad-auto.rb
+++ Formula/openscad-auto.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 class OpenscadAuto < Formula
-  desc "🟡 Run `openscad` on a `.scad` file with defaults."
+  desc "🟡 Run `openscad` to convert `.scad` file to `.stl` file with defaults."
   homepage "https://github.com/lgarron/scripts"
   head "https://github.com/lgarron/scripts.git", :branch => "main"
+
+  depends_on "terminal-notifier"
 
   def install
     bin.install "app-tools/openscad-auto.fish" => "openscad-auto"
