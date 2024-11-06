069cc7f0cbda29267ce7478f7f184a3e62884882
Lucas Garron <code@garron.net>
2024-11-05 18:11:04 -0800

    [chromium-bisect-builds] Try to pin the Python version to 3.11 to avoid a breaking change.

--- Formula/chromium-bisect-builds.rb
+++ Formula/chromium-bisect-builds.rb
@@ -4,6 +4,12 @@ class ChromiumBisectBuilds < Formula
   url "https://chromium.googlesource.com/chromium/src/+/HEAD/tools/bisect-builds.py?format=TEXT", using: :nounzip
   version "HEAD"
 
+  depends_on "python@3.10"
+
+  def python3
+    "python3.10"
+  end
+
   def install
     system "base64", "-d", "-i", "bisect-builds.py", "-o", "chromium-bisect-builds"
     chmod 0755, "chromium-bisect-builds"
