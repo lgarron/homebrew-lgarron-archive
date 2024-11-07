ab973776524381b4dd971405b659b57ea0b578c9
Lucas Garron <code@garron.net>
2024-11-06 17:32:10 -0800

    [tagpush] Test using the dotfiles repo and add completions.

--- Formula/tagpush.rb
+++ Formula/tagpush.rb
@@ -2,14 +2,17 @@
 
 class Tagpush < Formula
   desc "🏷️ Push and update `git` tags automatically."
-  homepage "https://github.com/lgarron/scripts"
-  head "https://github.com/lgarron/scripts.git", :branch => "main"
+  homepage "https://github.com/lgarron/dotfiles"
+  head "https://github.com/lgarron/dotfiles.git", :branch => "main"
 
   depends_on "lgarron/lgarron/rmtag"
   depends_on "lgarron/lgarron/version"
   depends_on "oven-sh/bun/bun"
 
   def install
-    bin.install "git/tagpush.ts" => "tagpush"
+    bin.install "scripts/git/tagpush.ts" => "tagpush"
+
+    # TODO: why can't `bin/"tagpush"` load deps when run directly?
+    # generate_completions_from_executable("bun", bin/"tagpush", "--completions", shells: [:fish])
   end
 end
